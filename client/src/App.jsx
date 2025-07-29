a05c5da8a7af2df1452a4dd589f9940556379842
AchyuthaVikaram <achyuthavikaram@gmail.com>
2025-07-29 18:06:16 +0530

    setup client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <p className="text-red-500">lorem</p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel inventore consequuntur sequi! Voluptatibus dicta ratione odio at quisquam accusamus! Provident recusandae ratione in unde accusamus temporibus ea ab sed accusantium.
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App;
